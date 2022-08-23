3886ce2f7c613f321c34462e8f2569b3381f3774
hyoje099 <hyoje099@naver.com>
2022-08-24 07:36:57 +0900

    시리얼 모니터 온습도 입력

--- lcd_dht/lcd_dht.ino
+++ lcd_dht/lcd_dht.ino
@@ -15,26 +15,51 @@ void setup() {
   lcd.init();
   lcd.backlight();
   lcd.setCursor(3,0);
-  lcd.print("220821");
+  char maker[12]={"makerishyoje"};
+  for(int i=0;i<12;i++){
+    lcd.setCursor(i,0);
+    lcd.print(maker[i]);
+  }
   lcd.setCursor(0,1);
   delay(2000);
   lcd.clear();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   delay(2000);
   int h=dht.readHumidity();
+  int h1=h/10;
+  int h2=h%10;
   int t=dht.readTemperature();
+  int t1=t/10;
+  int t2=t%10;
+  lcd.setCursor(0,0);
+  lcd.print("T");
+  lcd.print("e");
+  lcd.print("m");
+  lcd.print(t1);
+  lcd.print(t2);
+  lcd.print("C");
+  lcd.setCursor(0,1);
+  lcd.print("H");
+  lcd.print("u");
+  lcd.print("m");
+  lcd.print(h1);
+  lcd.print(h2);
+  /*
+  // put your main code here, to run repeatedly:
+  
 
-  Serial.print("Humidity:");
+  Serial.print("Hum:");
   Serial.print(h);
   Serial.print("%\t");
   Serial.print("Temperature:");
   Serial.print(t);
   Serial.print(" C");
   Serial.println(count);
-  lcd.setCursor(0,0);
+  
+  int x=0,y=0;
+  char printing[]
   lcd.print("Tem ");
   lcd.print(t);
   lcd.print("C");
@@ -42,12 +67,11 @@ void loop() {
   lcd.print(h);
   lcd.print("");
   lcd.setCursor(12,1);
-  lcd.print(count);
   delay(1);
   count+=1;
   if(count<9999){
     count=0;
     lcd.clear();
-  }
+  }*/
   
 }
